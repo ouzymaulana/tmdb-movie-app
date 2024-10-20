534db768121a3ad3ce81820ced3dfe254beb8029
ouzymaulana <ouzymaulana@gmail.com>
2024-10-20 20:22:34 +0700

    detail movie

--- src/pages/DetailMovie/DetailMovie.jsx
+++ src/pages/DetailMovie/DetailMovie.jsx
@@ -1,18 +1,35 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 import "./style.css";
+import { getMovieById } from "../../client";
+import bioskop from "./../../assets/bioskop1.svg";
 
-const DetailMoview = ({ movie }) => {
+const DetailMoview = () => {
+  const [detail, setDetail] = useState();
+  const { id } = useParams();
+
+  const handleGetDetailMovie = async () => {
+    const response = await getMovieById(id);
+    if (response.ok) {
+      setDetail(response.result);
+    }
+  };
+
+  useEffect(() => {
+    handleGetDetailMovie();
+  }, []);
   return (
-    <div className="movie-card flex">
-      <Link to={`/movie/${movie.id}`}>
-        <img
-          src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-          alt={movie.title}
-        />
-        <h2>{movie.title}</h2>
-        <p>Release Date: {movie.release_date}</p>
-      </Link>
+    <div
+      className="h-screen flex py-40"
+      style={{
+        backgroundImage: `url(${bioskop})`,
+        backgroundSize: "cover",
+        // backgroundPosition: "center",
+      }}
+    >
+      <div className="bg-slate-500 w-full rounded-3xl bg-opacity-75">
+        detail
+      </div>
     </div>
   );
 };
